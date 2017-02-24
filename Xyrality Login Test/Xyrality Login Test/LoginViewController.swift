43db98195d571cadd9c471d883fb5ce0158236bd
vitalii-tym <post.vitaliy@gmail.com>
2017-02-24 15:28:11 +0200

    Simple validation that the password and login are not empty and login is an email

--- Xyrality Login Test/Xyrality Login Test/LoginViewController.swift
+++ Xyrality Login Test/Xyrality Login Test/LoginViewController.swift
@@ -14,11 +14,22 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textFieldPassword: UITextField!
     @IBOutlet weak var buttonLogin: UIButton!
     
+    @IBAction func actionLogin(_ sender: UIButton) {
+        login()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         textFieldEmail.delegate = self
         textFieldPassword.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        textFieldPassword.addTarget(self, action: #selector(validateCredentialsAndEnableLogin), for: .allEvents)
+        textFieldEmail.addTarget(self, action: #selector(validateCredentialsAndEnableLogin), for: .allEvents)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,18 +39,40 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         if (textField.returnKeyType == UIReturnKeyType.next) && textField === textFieldEmail {
-                textFieldEmail.resignFirstResponder()
-                textFieldPassword.becomeFirstResponder()
+            textFieldEmail.resignFirstResponder()
+            textFieldPassword.becomeFirstResponder()
         } else if (textField.returnKeyType == UIReturnKeyType.done) && textField === textFieldPassword {
-
-            // TODO: Continue with the login process
-
+            textFieldPassword.resignFirstResponder()
+            login()
         } else {
             print("unexpected KeyType and textField pair. Check the advancing functionality on login screen")
         }
-        
-        return true
+        return false
     }
+    
+    func validateCredentialsAndEnableLogin() {
+        if let emailString = textFieldEmail.text, !emailString.isEmpty,
+            let passwordString = textFieldPassword.text, !passwordString.isEmpty,
+            emailString.contains("@") && emailString.contains(".") {
+            buttonLogin.isEnabled = true
+        } else {
+            buttonLogin.isEnabled = false
+        }
+    }
+    
+    func login() {
+
+        // TODO: Continue with the login process
+    
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        textFieldPassword.removeTarget(self, action: #selector(validateCredentialsAndEnableLogin), for: .valueChanged)
+        textFieldEmail.removeTarget(self, action: #selector(validateCredentialsAndEnableLogin), for: .valueChanged)
+        
+        super.viewWillDisappear(animated)
+    }
+    
     
 }
 
